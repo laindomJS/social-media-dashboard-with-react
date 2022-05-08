6a8a605fd28020a31246f7c03ceadfa38a394c25
Adrian Rangel <adrijar21@gmail.com>
2022-05-08 12:00:30 -0400

    cards responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './css/App.css';
 import iconF from './assets/images/icon-facebook.svg';
-// import iconI from './assets/images/icon-instagram.svg';
-// import iconT from './assets/images/icon-twitter.svg';
-// import iconY from './assets/images/icon-twitter.svg';
+import iconI from './assets/images/icon-instagram.svg';
+import iconT from './assets/images/icon-twitter.svg';
+import iconY from './assets/images/icon-twitter.svg';
 import iconUp from './assets/images/icon-up.svg';
-// import iconDown from './assets/images/icon-down.svg';
+import iconDown from './assets/images/icon-down.svg';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 
@@ -28,6 +28,41 @@ function App() {
           todayclassname = "todayNum up"
         />
 
+        <Card 
+          classname = "card twitter"
+          icon = {iconT}
+          name = "@nathanf"
+          followers = "1044"
+          cardContent = "FOLLOWERS"
+          todayIcon = {iconUp}
+          today = "99 Today"
+          todayclassname = "todayNum up"
+        />
+
+
+        <Card 
+          classname = "card insta"
+          icon = {iconI}
+          name = "@realnathanf"
+          followers = "11k"
+          cardContent = "FOLLOWERS"
+          todayIcon = {iconUp}
+          today = "1099 Today"
+          todayclassname = "todayNum up"
+        />
+      
+      
+        <Card 
+          classname = "card yt"
+          icon = {iconY}
+          name = "Nathan F."
+          followers = "8239"
+          cardContent = "SUSCRIBERS"
+          todayIcon = {iconDown}
+          today = "144 Today"
+          todayclassname = "todayNum down"
+        />
+      
       </main>
     </div>
   );
